7803390fd6c857bafe58126d83f5dce2efaf5fc4
Artem Holodyr <artemmholodyr@gmail.com>
2024-01-17 10:52:26 +0200

    database.py remove print statement

--- bot/db/database.py
+++ bot/db/database.py
@@ -9,7 +9,5 @@ url = f"postgresql+{config.db.drivername}://" \
       f"{config.db.user}:{config.db.password}@" \
       f"{config.db.host}:{config.db.port}" \
       f"/{config.db.database}"
-
-print(url)
 engine = create_async_engine(url=url, echo=True)
 sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
