b348bd61c71d9913c04b7281c98b08c0edc431ef
Artem Holodyr <artemmholodyr@gmail.com>
2024-01-17 10:40:27 +0200

    Rename __main__.py to main.py for convenience

---
+++ bot/main.py
@@ -0,0 +1,39 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from loguru import logger
+
+from config.config import config
+from handlers import handler
+from keyboards import set_main_menu
+from misc import redis
+from middlewares import DbSessionMiddleware
+from db import sessionmaker
+
+import asyncio
+
+
+async def main():
+    logger.add(
+        sink=config.logging.sink,
+        format=config.logging.format,
+        level=config.logging.level,
+        rotation=config.logging.rotation,
+        compression=config.logging.compression,
+        serialize=config.logging.serialize,
+    )
+    logger.info("Starting bot")
+
+    bot: Bot = Bot(token=config.bot.token, parse_mode=config.bot.parse_mode)
+    dp: Dispatcher = Dispatcher(storage=RedisStorage(redis))
+
+    dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
+
+    dp.include_router(handler.router)
+
+    await set_main_menu(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
